7b137b84503a341bd650bf8747e38b728af562e8
nicolasmq20 <nicolasmq@al.insper.edu.br>
2020-12-01 18:21:37 -0300

    colocar js em pasta

---
+++ JS/scripts.js
@@ -0,0 +1,19 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "80%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "white";
+  }
+
+function openNot() {
+    document.getElementById("myNotif").style.width = "80%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+function closeNot() {
+    document.getElementById("myNotif").style.width = "0";
+    document.body.style.backgroundColor = "white";
+  }
